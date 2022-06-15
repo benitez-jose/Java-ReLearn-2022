f1e7019120a33f2d0032d326644b1f0b14696831
Jose Benitez <jbenitez86@live.com>
2022-06-15 10:42:02 -0700

    new files

---
+++ Chapter 1/Algebra.java
@@ -0,0 +1,5 @@
+public class Algebra{
+    public static void main(String[] args){
+        
+    }
+}
---
+++ Chapter 1/AverageSpeedInKilometers.java
@@ -0,0 +1,13 @@
+public class AverageSpeedInKilometers{
+    public static void main(String[] args){
+        double miles = 24 * 1.6;
+        double kmh, hrs, minutes = 100.0;
+
+        hrs = 100.0 / 60.0;
+        //System.out.println("the hours is: " + hrs + ".");
+
+        kmh = miles / hrs;
+
+        System.out.println("The runners kmh is: " + kmh + ".");
+    }
+}
