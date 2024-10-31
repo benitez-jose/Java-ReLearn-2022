6cdadcdd980b6058ac3bf77ed1c115dfc4c1b187
Jose Benitez <jbenitez86@live.com>
2024-10-31 09:21:32 -0700

    added new code

--- Chapter 3/Problems/SolveQuadraticEquations.java
+++ Chapter 3/Problems/SolveQuadraticEquations.java
@@ -4,17 +4,25 @@ import java.util.Scanner;
 
 public class SolveQuadraticEquations {
     public static void main(String[] args){
-        int number1 = (int)(System.currentTimeMillis() % 10);
-        int number2 = (int)(System.currentTimeMillis() / 10 % 10);
-
         //Create a Scanner
         Scanner input = new Scanner(System.in);
 
-        System.out.print("What is " + number1 + " + " + number2 + "? ");
+        System.out.print("Enter a, b, c:  ");
 
-        int answer = input.nextInt();
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        double answer = Math.pow(b, 2) - (4*(a)*(c));
+        double squareAnswer = Math.pow(answer, 0.5);
 
-        System.out.println(number1 + " + " + number2 + " = " + answer + " is " +
-                (number1 + number2 == answer));
+        if (squareAnswer > 0){
+            System.out.println("The equation has 2 real roots.");
+        } else if (squareAnswer == 0) {
+            System.out.print("The equation has 1 real root.");
+        } else{ //if (squareAnswer < 0) {
+            System.out.println("The equation has no real roots.");
+        }
+
+        //System.out.println(a + " + " + b + " + " + c + " = " + answer);
     }
 }
