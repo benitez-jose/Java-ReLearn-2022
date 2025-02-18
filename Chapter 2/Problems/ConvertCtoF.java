0039a80c6be29cb18eb1fd366c6bed902cdec4be
Jose Benitez <jbenitez86@live.com>
2025-02-18 07:39:42 -0800

    finished converttof

--- Chapter 2/Problems/ConvertCtoF.java
+++ Chapter 2/Problems/ConvertCtoF.java
@@ -1,7 +1,17 @@
 package Problems;
 
+import java.util.*;
+
 public class ConvertCtoF{
     public static void main(String[] args){
-        System.out.println("hello this is a test");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a degree in Celsius: ");
+
+        double celsiusNum = input.nextDouble();
+
+        double fahConverted = ((9.0/5.0) * celsiusNum) + 32.0;
+
+        System.out.println(celsiusNum + " Celsius is " + fahConverted + " Fahrenheit");
     }
 }
